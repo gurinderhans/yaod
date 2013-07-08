b08c20c1653cc30ecb2e87c1bb36697829d8acac
Gurinder Hans <hansg@live.ca>
2013-07-08 16:49:12 -0700

    v1.06.2 Recreated the site structure, kept horizontal scroll, rearrangeable tiles

--- index.php
+++ index.php
@@ -6,46 +6,29 @@
 		</head>
 		<body>
 			<div id="droparea">
-				<div class="row" id="row1">
-					<div class="wideTile"></div>
-					<div class="smallTile"></div>
-					<div class="smallTile"></div>
-					<div class="wideTile"></div>
+				<div class="sortable" id="row1">
+					<div data-sizex="2" data-sizey="1" class="tile red"></div>
+					<div data-sizex="2" data-sizey="1" class="tile blue"></div>
+					<div data-sizex="2" data-sizey="1" class="tile green"></div>
 				</div>
-				<div class="row" id="row2">
-					<div class="wideTile"></div>
-					<div class="wideTile"></div>
-					<div class="wideTile"></div>
+				<div class="sortable" id="row2">
+					<div data-sizex="2" data-sizey="1" class="tile red"></div>
+					<div data-sizex="2" data-sizey="1" class="tile blue"></div>
+					<div data-sizex="2" data-sizey="1" class="tile green"></div>
 				</div>
-				<div class="row" id="row3">
-					<div class="smallTile"></div>
-					<div class="smallTile"></div>
-					<div class="wideTile"></div>
+				<div class="sortable" id="row3">
+					<div data-sizex="2" data-sizey="1" class="tile red"></div>
+					<div data-sizex="2" data-sizey="1" class="tile blue"></div>
+					<div data-sizex="2" data-sizey="1" class="tile green"></div>
 				</div>
-				<div id="message"></div>
 			</div>
-			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script><!--Normal jQuery-->
+			<div id="message"></div>
+			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script><!--jQuery-->
 			<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script><!--jQuery UI-->
 			<script type="text/javascript" src="js/fileUpload.js"></script>
 			<script type="text/javascript" src="js/dashboard.js"></script>
 			<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
-			<script>/*
-			var mouseWheelEvt = function (e){
-				var event = e || window.event;
-				if (document.body.doScroll){
-					document.body.doScroll(event.wheelDelta>0?"left":"right");
-				} else if ((event.wheelDelta || event.detail) > 0){
-					document.body.scrollLeft -= 110;
-				} else{
-					document.body.scrollLeft += 110;
-				}
-				return false;
-			}
-			if ("onmousewheel" in document.body){
-				document.body.onmousewheel = mouseWheelEvt;
-			} else{
-				document.body.addEventListener("DOMMouseScroll", mouseWheelEvt);
-			}*/
+			<script>
 			$(function(){
 				$("body").mousewheel(function(event, delta) {
 					this.scrollLeft -= (delta * 120);
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,13 +1,85 @@
-var windowHeight = $(window).height();
-var middleSpace = (1/111) * windowHeight;
-var row1Top = (1/12) * windowHeight;
-var rowHeight = (windowHeight - ((row1Top*2)+(middleSpace*2)))/3;
-$(".row").height(rowHeight+"px");
-$("#row1").css("top",row1Top+"px");
-var row2Top = row1Top + middleSpace;
-$("#row2").css("top",row2Top+"px");
-var row3Top = row2Top + middleSpace;
-$("#row3").css("top",row3Top+"px");
-$("#droparea").click(function(e){
-	alert("clicked");
+/*
+*
+*
+*I am hoping to properly comment this very soon. Thanks :)
+*
+*/
+
+$(".sortable").height($('.tile').height()+"px");
+/*Row 1 width*/
+var row1Width = 0;
+var numTilesInRow1 = $("#row1 div").length;
+$("#row1 div").each(function(index){
+	row1Width = $(this).width() * numTilesInRow1+100;
+});
+$("#row1").width(row1Width+"px");
+/*Row 2 width*/
+var row2Width = 0;
+var numTilesInRow2 = $("#row2 div").length;
+$("#row2 div").each(function(index){
+	row2Width = $(this).width() * numTilesInRow2+100;
+});
+$("#row2").width(row2Width+"px");
+/*Row 3 width*/
+var row3Width = 0;
+var numTilesInRow3 = $("#row3 div").length;
+$("#row3 div").each(function(index){
+	row3Width = $(this).width() * numTilesInRow3+100;
+});
+$("#row3").width(row3Width+"px");
+
+
+$(function() {
+	$( ".sortable" ).sortable({
+		revert: 400,
+		scroll: true,
+		connectWith: ".sortable",
+		change: function( event, ui ) {
+			/*Row 1 width*/
+			var row1Width = 0;
+			var numTilesInRow1 = $("#row1 div").length;
+			$("#row1 div").each(function(index){
+				row1Width = $(this).width() * numTilesInRow1+100;
+			});
+			$("#row1").width(row1Width+"px");
+			/*Row 2 width*/
+			var row2Width = 0;
+			var numTilesInRow2 = $("#row2 div").length;
+			$("#row2 div").each(function(index){
+				row2Width = $(this).width() * numTilesInRow2+100;
+			});
+			$("#row2").width(row2Width+"px");
+			/*Row 3 width*/
+			var row3Width = 0;
+			var numTilesInRow3 = $("#row3 div").length;
+			$("#row3 div").each(function(index){
+				row3Width = $(this).width() * numTilesInRow3+100;
+			});
+			$("#row3").width(row3Width+"px");
+		},
+		stop: function( event, ui ) {
+			/*Row 1 width*/
+			var row1Width = 0;
+			var numTilesInRow1 = $("#row1 div").length;
+			$("#row1 div").each(function(index){
+				row1Width = $(this).width() * numTilesInRow1+100;
+			});
+			$("#row1").width(row1Width+"px");
+			/*Row 2 width*/
+			var row2Width = 0;
+			var numTilesInRow2 = $("#row2 div").length;
+			$("#row2 div").each(function(index){
+				row2Width = $(this).width() * numTilesInRow2+100;
+			});
+			$("#row2").width(row2Width+"px");
+			/*Row 3 width*/
+			var row3Width = 0;
+			var numTilesInRow3 = $("#row3 div").length;
+			$("#row3 div").each(function(index){
+				row3Width = $(this).width() * numTilesInRow3+100;
+			});
+			$("#row3").width(row3Width+"px");
+		}
+	});
+	$( ".sortable" ).disableSelection();
 });
