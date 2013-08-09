0d080de2b4ceb31e87a2ab36edcf0c59975d1428
Gurinder Hans <hello@gurinderhans.me>
2013-08-09 17:05:07 +0000

    v1.0716-85 fixed calendar tileUpdater.js. Calendar now updates properly. We can call this a stable yaod v1.0716-85

--- js/tileUpdater.js
+++ js/tileUpdater.js
@@ -2,31 +2,23 @@
 var numCalendarEvents = $(".calendar p").size();
 $(".calendar > #1").show().delay(8000).hide("slide", { direction: "left" }, 200);
 var nextEvent = 2;
-var direction;
-var otherDirection;
+var dirShow = "";
+var dirHide = "";
+var randomNum;
 var a=setInterval(function(){updateCalendar()},8400); 
 function updateCalendar(){
-    var randomNum = Math.random();
-    if(randomNum > 0.5){
-        directionID = 1;
-    } else{
-        directionID = 0;
+    randomNum = Math.floor(Math.random()*2);
+    if(randomNum === 1){
+        dirShow = "left";
+        dirHide = "right";
+    } else if(randomNum === 0){
+        dirShow = "right";
+        dirHide = "left";
     }
-    if(directionID = 1){
-        direction = "right";
-    } else{
-        direction = "left";
-    }
-    if(direction == "right"){
-        otherDirection = "left";
-    } else if(direction == "left"){
-        otherDirection = 'right';
-    }
-    console.log(randomNum);
     if(nextEvent > numCalendarEvents){
         nextEvent = 1;
     }
-    $(".calendar > #"+nextEvent).show("slide", { direction: ""+direction+"" }, 200).delay(8000).hide("slide", { direction: ""+otherDirection+"" }, 200);
+    $(".calendar > #"+nextEvent).show("slide", { direction: ""+dirShow+"" }, 200).delay(8000).hide("slide", { direction: ""+dirHide+"" }, 200);
     if(nextEvent == numCalendarEvents){
         nextEvent = 1;
     } else{
